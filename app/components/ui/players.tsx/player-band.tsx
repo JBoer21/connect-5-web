a1f0cf2a5cae3a9e4d63dc9581cdbeba3f2c56cb
Jehan <boerjehan@gmail.com>
2024-07-29 21:14:01 -0400

    player-band defaults all true [for now]

--- app/components/ui/players.tsx/player-band.tsx
+++ app/components/ui/players.tsx/player-band.tsx
@@ -13,7 +13,12 @@ export function PlayerBand({ players }: PlayerRowProps) {
   return (
     <div className="flex items-center justify-center gap-2 p-4 overflow-x-auto">
       {players.slice(0, 5).map((player, index) => (
-        <PlayerCard key={index} name={player.name} imageUrl={player.imageUrl} />
+        <PlayerCard
+          key={index}
+          name={player.name}
+          imageUrl={player.imageUrl}
+          show={true}
+        />
       ))}
     </div>
   );
