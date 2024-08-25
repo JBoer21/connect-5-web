c89b411e0b6fff09767b9f49cf2f62d078538520
Jehan <boerjehan@gmail.com>
2024-08-25 14:01:47 -0400

    more logic changes

--- app/lib/utils/index_utils.ts
+++ app/lib/utils/index_utils.ts
@@ -11,42 +11,6 @@ export function getGameStateForDate(date: Date): number {
   return dayDiff + 1; // Add 1 to start from game state 1
 }
 
-// export function setGame() {
-//   const typedData: ClubData = data as ClubData;
-
-//   let teamNames = getAvailableTeams();
-
-//   console.log(teamNames.length);
-
-//   // If all teams have been used, reset the available teams
-//   if (teamNames.length === 0) {
-//     resetTeams();
-//     teamNames = getAvailableTeams();
-//   }
-
-//   const randomTeamName =
-//     teamNames[Math.floor(Math.random() * teamNames.length)];
-//   const randomTeam = typedData[randomTeamName];
-
-//   // Remove the selected team from available teams
-//   removeTeam(randomTeamName);
-
-//   const allPlayers = randomTeam.players;
-//   const shuffledPlayers = [...allPlayers].sort(() => 0.5 - Math.random());
-
-//   const selectedPlayers = shuffledPlayers.slice(0, 5);
-
-//   const sortedPlayers = selectedPlayers.sort(
-//     (a, b) => b.num_clubs - a.num_clubs,
-//   );
-
-//   return json<IndexLoaderData>({
-//     teamName: randomTeamName,
-//     teamLogo: randomTeam.logo,
-//     players: sortedPlayers,
-//   });
-// }
-
 export function setGame() {
   const currentDate = new Date();
   const gameStateId = getGameStateForDate(currentDate);
