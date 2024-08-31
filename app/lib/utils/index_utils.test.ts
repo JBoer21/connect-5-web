5ed16d86faa6ac816b85c967b93ac86b420b9199
Jehan <boerjehan@gmail.com>
2024-08-31 14:34:36 -0400

    updated tests

--- app/lib/utils/index_utils.test.ts
+++ app/lib/utils/index_utils.test.ts
@@ -6,14 +6,14 @@ jest.mock("@remix-run/node", () => ({
 
 describe("getGameStateForDate", () => {
   test("returns correct game state for the base date", () => {
-    const baseDate = new Date("2024-08-26");
+    const baseDate = new Date("2024-08-31");
     expect(getGameStateForDate(baseDate)).toBe(1);
   });
 
   test("returns incremented game state for each day after base date", () => {
-    const baseDate = new Date("2024-08-26");
-    const nextDay = new Date("2024-08-27");
-    const tenDaysLater = new Date("2024-09-05");
+    const baseDate = new Date("2024-08-31");
+    const nextDay = new Date("2024-09-01");
+    const tenDaysLater = new Date("2024-09-10");
 
     expect(getGameStateForDate(baseDate)).toBe(1);
     expect(getGameStateForDate(nextDay)).toBe(2);
@@ -21,12 +21,12 @@ describe("getGameStateForDate", () => {
   });
 
   test("handles dates before the base date", () => {
-    const earlierDate = new Date("2024-08-25");
+    const earlierDate = new Date("2024-08-30");
     expect(getGameStateForDate(earlierDate)).toBe(0);
   });
 
   test("handles year change correctly", () => {
-    const baseDate = new Date("2024-08-26");
+    const baseDate = new Date("2024-08-31");
     const yearChangeDate = new Date("2025-01-01");
     const daysDifference = Math.floor(
       (yearChangeDate.getTime() - baseDate.getTime()) / (1000 * 60 * 60 * 24),
