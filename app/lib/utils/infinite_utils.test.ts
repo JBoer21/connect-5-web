2d878c0cd4152b3baa5543fd9a91cae6e48cc741
Jehan <boerjehan@gmail.com>
2024-10-13 13:48:56 -0400

    added test workflow

--- app/lib/utils/infinite_utils.test.ts
+++
@@ -1,8 +0,0 @@
-import { createRandomGameState } from "./infinite_utils";
-
-describe("createRandomGameState", () => {
-  it("should log the result of createRandomGameState", () => {
-    const result = createRandomGameState();
-    console.log(result);
-  });
-});
